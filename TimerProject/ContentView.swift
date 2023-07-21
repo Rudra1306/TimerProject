e8f1d768a4998dfcc7f5ed72145a7d03c8f5b0a0
Rudra1306 <rudraparikh6@gmail.com>
2023-07-21 21:51:09 +1000

    Done in one go pretty simple.

--- TimerProject/ContentView.swift
+++ TimerProject/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var vm = ViewModel()
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    private let width: Double = 250
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(vm.time)")
+                .font(.system(size: 70, weight: .medium, design: .rounded))
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(20)
+                .overlay(RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.gray, lineWidth: 4))
+                .frame(width: width)
+                .alert("Timer Done", isPresented: $vm.showingAlert) {
+                    Button("Continue", role: .cancel) {
+                        //
+                    }
+                }
+            Slider(value: $vm.minutes, in: 0...25, step: 5)
+                .padding()
+                .frame(width: width)
+                .disabled(vm.isActive)
+                .animation(.easeInOut, value: vm.minutes)
+            
+            HStack(spacing: 50) {
+                Button("Start") {
+                    vm.start(minutes: vm.minutes)
+                }
+                .disabled(vm.isActive)
+                
+                Button("Reset", action: vm.reset)
+                    .tint(.red)
+            }
+            .frame(width: width)
         }
-        .padding()
+        .onReceive(timer) { _ in
+            vm.updateCountdown()
+        }
     }
 }
 
