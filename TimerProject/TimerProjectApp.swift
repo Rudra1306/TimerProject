7eedcd462a400f3d51663c6911006bf7dd6c40d6
Rudra1306 <rudraparikh6@gmail.com>
2023-07-21 16:01:16 +1000

    Initial Commit

---
+++ TimerProject/TimerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerProjectApp.swift
+//  TimerProject
+//
+//  Created by Rudra on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
